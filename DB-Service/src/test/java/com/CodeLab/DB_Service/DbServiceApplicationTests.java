42a4152f273860384caf194348a478cf75954776
Ansari Mohd Saad <ansarisaad777666@gmail.com>
2025-05-03 14:30:55 +0530

    Just created the DB-Service from the Spring Initializr

---
+++ DB-Service/src/test/java/com/CodeLab/DB_Service/DbServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CodeLab.DB_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
